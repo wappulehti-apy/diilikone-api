46220c1e785d15483cdbd9c8bd5d85d7b7d23b3d
Vesa Uimonen <vesa.uimonen@gmail.com>
2015-02-06 23:04:20 +0200

    Make shell context

--- manage.py
+++ manage.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from flask import current_app
 from flask.ext.failsafe import failsafe
 from flask.ext.script import Manager, Server
 
@@ -12,6 +13,20 @@ def create_app():
 
 manager = Manager(create_app)
 manager.add_command('runserver', Server(host='localhost'))
+
+
+@manager.shell
+def make_shell_context():
+    from diilikone.extensions import db
+    from diilikone import load_models
+
+    load_models()
+    context = {}
+    context['app'] = current_app
+    context['db'] = db
+    context.update(db.Model._decl_class_registry)
+
+    return context
 
 if __name__ == '__main__':
     manager.run()
