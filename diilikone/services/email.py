0674827cd25052e9c7ccad347084bf3164edcfca
Samuli Suortti <samuli.suortti@gmail.com>
2015-03-25 22:06:49 +0200

    Small email sent refactoring

--- diilikone/services/email.py
+++ diilikone/services/email.py
@@ -1,8 +1,27 @@
+# -*- coding: utf-8 -*-
 from flask import render_template
 from flask_mail import Message
 
 from diilikone.extensions import mail
 from diilikone.models import Deal, IndividualProvision, GroupProvision
+
+
+def _calculateIndividualProvision(deal):
+    provision = IndividualProvision.query.filter_by(quantity=deal.size).one()
+    base = provision.price_per_magazine
+    for product in deal.product_types:
+        base -= product.price / deal.size
+    return base
+
+
+def _get_group_provision(group):
+    if group:
+        group_provision = GroupProvision.query.filter_by(
+            quantity=group.total_size
+        ).first()
+        return group_provision.price_per_magazine
+    else:
+        return 0
 
 
 def send_confirmation_email(deal):
@@ -14,12 +33,8 @@ def send_confirmation_email(deal):
         bcc=["diilit@apy.fi"]
     )
 
-    provision = IndividualProvision.query.filter_by(quantity=deal.size).one()
-
-
-    group_provision = GroupProvision.query.filter_by(
-        quantity=deal.deal_group.total_size
-    ).first()
+    provision = _calculateIndividualProvision(deal)
+    group_provision = _get_group_provision(deal.deal_group)
 
     msg.body = render_template(
         'confirmation_email.txt',
