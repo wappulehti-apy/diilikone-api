66092fb6419db0fef207b5bb4a9cb96efc05ac74
Samuli Suortti <samuli.suortti@gmail.com>
2017-01-27 09:32:36 +0200

    Refactor application settings

--- diilikone/settings/development.py
+++ diilikone/settings/development.py
@@ -1,26 +1,11 @@
 import os
 
+from .base import *  # flake8: noqa
+
 SECRET_KEY = 'development_key'
 
 WTF_CSRF_ENABLED = False
 
 DEBUG = True
 
-SQLALCHEMY_DATABASE_URI = os.environ.get(
-    'DATABASE_URL',
-    'postgres://localhost/diilikone'
-)
-CORS_HEADERS = 'Content-Type'
-
 MAIL_SUPPRESS_SEND = True
-
-MAIL_SERVER = os.environ.get('MAIL_SERVER')
-
-MAIL_PORT = int(os.environ.get('MAIL_PORT'))
-
-MAIL_USERNAME = os.environ.get('MAIL_USER')
-
-MAIL_PASSWORD = os.environ.get('MAIL_KEY')
-
-MAIL_DEFAULT_SENDER = os.environ.get('MAIL_SENDER')
-MAIL_USE_TLS = bool(os.environ.get('MAIL_USE_TLS'))
