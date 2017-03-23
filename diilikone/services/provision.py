4063dbf407274504a94bc5dce3ad7c56da666f02
Aleksi Pyykkönen <aleksi.pyykkonen@aalto.fi>
2017-03-23 20:53:59 +0200

    Fix provision calculation

--- diilikone/services/provision.py
+++ diilikone/services/provision.py
@@ -29,6 +29,14 @@ def calculate_total_price_of_products(product_ids):
         return total_prices
 
 
+def calculate_group_cut(group_id, size):
+    group_part = calculate_group_part(group_id, size)
+    if group_part == 0:
+        return 0
+    group = DealGroup.query.get(group_id)
+    return (group_part * group.percentage * size) / 100
+
+
 def calculate(data):
     size = data.get('size')
     group_id = data.get('group_id', None)
@@ -37,11 +45,15 @@ def calculate(data):
     individual_part = calculate_individual_part(size)
     group_part = calculate_group_part(group_id, size)
     products_price = calculate_total_price_of_products(product_ids)
+    guild_cut = calculate_group_cut(group_id, size)
 
-    total_provision = (individual_part + group_part) * size - products_price
+    total_provision = (
+        (individual_part + group_part) * size - products_price - guild_cut
+    )
     per_magazine = total_provision / size
 
     return {
       'total': round(total_provision, 2),
-      'per_magazine': round(per_magazine, 2)
+      'per_magazine': round(per_magazine, 2),
+      'group': round(guild_cut, 2)
     }
