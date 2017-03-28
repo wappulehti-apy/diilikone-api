493e5980485ca8f0eda10ba578fe7307f0fb66e4
Samuli Suortti <samuli.suortti@gmail.com>
2017-03-28 14:36:00 +0300

    Expose target description to deal group endpoint

--- diilikone/views/deal_groups.py
+++ diilikone/views/deal_groups.py
@@ -8,5 +8,14 @@ deal_groups = Blueprint('deal_groups', __name__, url_prefix='/deal-groups')
 @deal_groups.route('')
 def index():
     deal_groups = DealGroup.query.order_by('name')
-    data = [{'id': str(group.id), 'name': group.name} for group in deal_groups]
+    data = [
+        {
+            'id': str(group.id),
+            'name': group.name,
+            'target_description': group.target_description,
+            'percentage': group.percentage
+
+        }
+        for group in deal_groups
+    ]
     return jsonify({'data': data}), 200
