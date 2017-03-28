7e3fbb896334164fe5fca1550db5040e87d87731
Aleksi Pyykkönen <aleksi.pyykkonen@aalto.fi>
2017-03-28 16:21:39 +0300

    Fix deal status endpoint

--- diilikone/views/deal_status.py
+++ diilikone/views/deal_status.py
@@ -13,20 +13,18 @@ deal_status = Blueprint(
 def index(secret_id):
     deal_group = DealGroup.query.filter_by(secret_id=secret_id).first_or_404()
 
-    data = [
-        {
-            'group_name': deal_group.name,
-            'total_size': deal_group.total_size,
-            'total_money': deal_group.total_money,
-            'percentage': deal_group.percentage,
-            'target_description': deal_group.target_description,
-            'salespersons': [
-                {
-                    'name': deal.salesperson.name,
-                    'size': deal.size
-                } for deal in deal_group.deals
-            ]
-        }
-    ]
+    data = {
+        'group_name': deal_group.name,
+        'total_size': deal_group.total_size,
+        'total_money': deal_group.total_money,
+        'percentage': deal_group.percentage,
+        'target_description': deal_group.target_description,
+        'salespersons': [
+            {
+                'name': deal.salesperson.name,
+                'size': deal.size
+            } for deal in deal_group.deals
+        ]
+    }
 
     return jsonify({'data': data}), 200
