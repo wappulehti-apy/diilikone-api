49c419745d164c2c1776d353bf57a14abf260d7a
Aleksi Pyykkönen <aleksi.pyykkonen@aalto.fi>
2017-04-07 00:03:02 +0300

    Add latest deals to admin status view

--- diilikone/services/overall.py
+++ diilikone/services/overall.py
@@ -7,7 +7,7 @@ from diilikone.models import (
 
 def get_status_data():
     groups = DealGroup.query.all()
-    deals = Deal.query.all()
+    deals = Deal.query.order_by(Deal.created_at.desc()).all()
     products = ProductType.query.all()
 
     total_sum = sum(deal.size for deal in deals)
@@ -15,11 +15,13 @@ def get_status_data():
         groups, key=lambda deal_group: deal_group.total_size,
         reverse=True
     )
+    new_deals = deals[:10]
 
     data = {
         'total_size': total_sum,
         'groups': sorted_groups,
         'salespersons': len(deals),
-        'products': products
+        'products': products,
+        'new_deals': new_deals
     }
     return data
