d09429a05e1ce8865d1c9165fd501a637793be97
Aleksi Pyykkönen <aleksi.pyykkonen@aalto.fi>
2017-04-17 13:01:13 +0300

    Add fetched status to admin status view

--- diilikone/services/overall.py
+++ diilikone/services/overall.py
@@ -9,8 +9,24 @@ def get_status_data():
     groups = DealGroup.query.all()
     deals = Deal.query.order_by(Deal.created_at.desc()).all()
     products = ProductType.query.all()
-
     total_sum = sum(deal.size for deal in deals)
+
+    # get deals with backpacks
+    deals_with_backpacks = ProductType.query.filter_by(
+        name='Reppu').first().deals
+
+    # of those deals, how many have NOT fetched backpack
+    backpacks_to_be_fetched = sum(
+        1 for d in deals_with_backpacks if not d.backpack_received)
+
+    # get deals with products with name shirt
+    deals_with_shirts = Deal.query.join(
+        Deal.product_types).filter(ProductType.name.ilike("%paita%")).all()
+
+    # calculate deals that have NOT received shirt
+    shirts_to_be_fetched = sum(
+        1 for d in deals_with_shirts if not d.shirt_received)
+
     sorted_groups = sorted(
         groups, key=lambda deal_group: deal_group.total_size,
         reverse=True
@@ -22,6 +38,10 @@ def get_status_data():
         'groups': sorted_groups,
         'salespersons': len(deals),
         'products': products,
-        'new_deals': new_deals
+        'new_deals': new_deals,
+        'shirts_to_be_fetched': shirts_to_be_fetched,
+        'backpacks_to_be_fetched': backpacks_to_be_fetched,
+        'total_shirts': len(deals_with_shirts),
+        'total_backpacks': len(deals_with_backpacks),
     }
     return data
